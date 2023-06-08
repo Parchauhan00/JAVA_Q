d29e39778cf880c935f4ea700805af79991c8314
Prashant Chauhan <vipchauhan00@gmail.com>
2023-06-09 00:53:36 +0530

    Add files via upload

---
+++ LinkedList/Linklistcf.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Linklistcf {
+    public static void main(String args []) {
+        LinkedList<String >list = new LinkedList<>();
+        list.add("P");
+        list.add("R");
+        list.add("I");
+        list.add("M");
+        list.add("E");
+        System.out.println(list);
+
+    }
+}
