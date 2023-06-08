7cad64352adf101efadd5c83ebf174c0777e2277
Prashant Chauhan <vipchauhan00@gmail.com>
2023-06-09 00:32:16 +0530

    Add files via upload

---
+++ Hashing/Find_ItineraryFrom_Tickets.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class Find_ItineraryFrom_Tickets {
+
+    public static String getStart(HashMap<String, String>tickets) {
+        HashMap<String , String > revMap = new HashMap<>();
+
+        for (String key : tickets.keySet()) {
+            revMap.put(tickets.get(key), key);
+        }
+
+        for (String key : tickets.keySet()) {
+            if (!revMap.containsKey(key) ) {
+                return key; // Starting point
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+
+        HashMap<String, String> tickets = new HashMap<>();   //O(N)
+        tickets.put("Chennai ", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getStart(tickets);
+        System.out.println(start);
+        for (String k : tickets.keySet()) {
+            System.out.print("->" + tickets.get(start));
+            start = tickets.get(start);
+        }
+        System.out.println();
+
+    }
+}
